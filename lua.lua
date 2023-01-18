69c3f350c6a47639d30b4515e5fad480af5ff1e8
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-01-17 18:53:25 -0700

    growing animal

--- lua.lua
+++ lua.lua
@@ -1,10 +1,14 @@
-if sucess and hum.Health>0 then
-			local ls = player:FindFirstChild("leaderstats");
-			if ls then
-				local Level = ls:FindFirstChild("Level");
-				if EXP.Value == 25 then
-					Level.Value = Level.Value +1
-					print("Leveled up!")
-				end
-			end
-		end
+local animal = script.Parent --Assuming that the script is located in the animal object
+local food = script:WaitForChild("Food") --Assuming that the food value is a child of the script
+
+if success and animal.Health > 0 then
+    local Level = animal:FindFirstChild("Level")
+    if Level then
+        if food.Value >= 25 then
+            food.Value = food.Value - 25
+            Level.Value = Level.Value + 1
+            animal.Size = animal.Size * 2 -- Double the size of the animal
+            print("Animal Leveled up!")
+        end
+    end
+end
